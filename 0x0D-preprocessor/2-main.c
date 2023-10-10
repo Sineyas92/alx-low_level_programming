8e24e3128d08ac79bd99bf1b15537902e941e2a3
Sineyas92 <yssine.lchhb@gmail.com>
2023-10-10 16:54:44 -0700

    full task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,12 @@
-#ifndef _HEADER_
-#define _HEADER_
-#define PI 3.14159265359
-#endif
+#include <stdio.h>
+
+/**
+  * main - Prints the file name of the program
+  *
+  * Return: Always zero
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
