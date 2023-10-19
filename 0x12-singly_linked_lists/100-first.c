5c85a9319485c598cc4d84d7fb7a5fee84c2cbf7
Sineyas92 <yssine.lchhb@gmail.com>
2023-10-19 14:29:21 -0700

    addi task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,4 +9,5 @@ void first(void) __attribute__ ((constructor));
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore m
+	printf("I bore my house upon my back!\n");
+}
